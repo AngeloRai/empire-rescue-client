a869ead2ce2dd3b7f679d40651d0c7d85ccb7c8a
Angelo Raimondi <angelo.raimondi@globalsys.com.br>
2022-03-30 14:39:49 -0700

    added demo user info to home page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,21 +1,26 @@
-import React from 'react'
-import bombeiros from '../../images/bombeiros-450px.png'
-import cruz from '../../images/cruz-de-viga-450px.png'
-import medical from '../../images/empiremedical-450px.png'
-import gh from '../../images/gestao-hospitalar-450px.png'
-
+import React from "react";
+import bombeiros from "../../images/bombeiros-450px.png";
+import cruz from "../../images/cruz-de-viga-450px.png";
+import medical from "../../images/empiremedical-450px.png";
+import gh from "../../images/gestao-hospitalar-450px.png";
 
 function Home() {
   return (
     <div className="w-100 h-100 row">
       <div className="row d-flex justify-content-center align-items-center gap-3">
-        <img className="col-7 col-md-2" src={bombeiros} alt="firefighter"/>
-        <img className="col-7 col-md-2" src={cruz} alt="cross"/>
-        <img className="col-7 col-md-2" src={medical} alt="medical"/>
-        <img className="col-7 col-md-2" src={gh} alt="mangement"/>
+        <br />
+        <small>
+          <strong>demo user</strong> email: teste@teste.com password: teste123
+        </small>
+        <br />
+
+        <img className="col-7 col-md-2" src={bombeiros} alt="firefighter" />
+        <img className="col-7 col-md-2" src={cruz} alt="cross" />
+        <img className="col-7 col-md-2" src={medical} alt="medical" />
+        <img className="col-7 col-md-2" src={gh} alt="mangement" />
       </div>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
