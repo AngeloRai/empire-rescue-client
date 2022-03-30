86213c9105d158c3b8ab604bd6333f4d8a7dc4a5
Angelo Raimondi <angelo.raimondi@globalsys.com.br>
2022-03-30 15:14:32 -0700

    adjusted demo user info home page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -8,10 +8,9 @@ function Home() {
   return (
     <div className="w-100 h-100 row">
       <small>
-        <strong className="m-3">demo user</strong> email: teste@teste.com
-        password: teste123
+        <strong>demo user</strong> email: teste@teste.com password: teste123
       </small>
-      <div className="row d-flex justify-content-center gap-3">
+      <div className="row d-flex justify-content-center align-items-center gap-3">
         <img className="col-7 col-md-2" src={bombeiros} alt="firefighter" />
         <img className="col-7 col-md-2" src={cruz} alt="cross" />
         <img className="col-7 col-md-2" src={medical} alt="medical" />
